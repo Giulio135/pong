b26ef5b8f2e0a2cdd8e98f47b89684e955f18e62
Giulio135 <kilokoloni@gmail.com>
2025-05-21 18:32:09 +0200

    Pong.py

--- Pong.py
+++ Pong.py
@@ -1,17 +1,5 @@
 import pygame
 
-class Player1(pygame.sprite.Sprite):
-    def __init__(self, groups):
-        super().__init__(groups)
-        self.image = 
-        self.x = DISPLAY_W + 10
-        self.y = DISPLAY_H / 2
-        self.rect = self.image.get_frect(center=(self.x, self.y))
-        self.direction = pygame.math.Vector2()
-        self.speed = 10
-    def update(self):
-        pass
-
 DISPLAY_W = 500
 DISPLAY_H = 500
 running = True
@@ -19,8 +7,6 @@ running = True
 all_sprites = pygame.sprite.Group()
 
 display_surf = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
-player1 = Player1(all_sprites)
-
 
 while running:
 
@@ -29,8 +15,6 @@ while running:
             running = False
 
     display_surf.fill('white')
-    all_sprites.draw(display_surf)
-    all_sprites.update()
     pygame.display.update()
 
 pygame.quit()
